a6d5b5779e95bacbe397500230cb41259fa46a85
Evgen <evgen.knyzhenko@gmail.com>
2018-05-23 13:57:09 +0300

    LRU

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,26 @@ public class Main {
         LRUCache cache = new LRUCache();
 
         cache.put(11, 111);
+        cache.put(12,222);
+        cache.put(13, 333);
+        cache.put(14,444);
+        cache.put(15, 555);
 
+        cache.put(16,666);
+        cache.put(17, 777);
+        cache.put(18,888);
 
         int result = cache.get(14);
 
         cache.put(19,999);
 
+        int result2 = cache.get(15);
 
 
 
 
         System.out.println(result);
-        //System.out.println(result2);
+        System.out.println(result2);
 
     }
 }
